b8ea3caea4f420084068ed0f69e0de4d603e63cb
Hailey Montgomery <hmontgomery@ggc.edu>
2017-02-14 12:44:39 -0500

    added all necessary foreign keys

---
+++ bookswap/database/migrations/2017_02_14_173648_create_exchange_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExchangeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('books_id')->unsigned();
+            $table->foreign('books_id')->references('id')->on('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange');
+    }
+}

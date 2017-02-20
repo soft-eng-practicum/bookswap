28f8392d74d3925c7fa7a98f94acd849254643ec
Hailey Montgomery <hmontgomery@ggc.edu>
2017-02-20 17:37:50 -0500

    changed selling and exchange html

--- bookswap/vendor/autoload.php
+++ bookswap/vendor/autoload.php
@@ -4,6 +4,6 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-require_once '/Users/Maestro/Desktop/bookswap/bookswap/vendor/autoload.php';
+require_once 'C:/Users/hailO/bookswap/bookswap/vendor/autoload.php';
 
 return ComposerAutoloaderInitb0c2699e8af7dbed83db08ab1a6eafb6::getLoader();

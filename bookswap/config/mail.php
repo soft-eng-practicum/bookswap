4ef9bfd8bb99278cf9511978072fc9f478cdb840
Hailey Montgomery <hmontgomery@ggc.edu>
2017-04-19 11:41:37 -0400

    changed email address

--- bookswap/config/mail.php
+++ bookswap/config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'bookswap@no-reply.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bookswapy@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Bookswap'),
     ],
 

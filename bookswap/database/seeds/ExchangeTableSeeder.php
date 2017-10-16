8860ca12e5db71ad638cb9e474ca2fc37587b099
Chelsea D'Alessandro <cdalessandro@ggc.edu>
2017-10-16 12:26:22 -0400

    Edited ExchangeTableSeeder.php after speaking to Dr Gunay in class.  He pointed out there was only 1 user, so user_id => 1

--- bookswap/database/seeds/ExchangeTableSeeder.php
+++ bookswap/database/seeds/ExchangeTableSeeder.php
@@ -17,7 +17,7 @@ class ExchangeTableSeeder extends Seeder
             'desc' => 'fair',
 			'description' => 'Slightly used',
 			'price' => '22.22',
-			'user_id' => '4',
+			'user_id' => '1',
 			'books_id' => '2'
 		]);
     }

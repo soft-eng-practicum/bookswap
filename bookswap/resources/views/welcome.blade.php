23badd39bb789c6a4907b5d68db53fc00736cfcc
Chelsea D'Alessandro <cdalessandro@ggc.edu>
2017-10-16 11:48:30 -0400

    Added 2 bookswap logo pngs (for transparent bg), logo.png and logo-big.png. Redesigned wlecome page, welcome.blade.php. Changed import fonts, css styles, logo image and font, and navigation (top and bottom) Moved nav -> header & footer. Header, main, and footer now reflect mockup. Added several comments. Got rid of needless spacing. Fixed indentation.

--- bookswap/resources/views/welcome.blade.php
+++ bookswap/resources/views/welcome.blade.php
@@ -1,266 +1,220 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- IMPORT: image (bookswap logo)
+    NOTE: unused... remove.
+    NOTE: make book logo to return to home page -->
+    <link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
 
-        <title>Book Swap: Main Page</title>
+    <!-- IMPORT: Stylesheets -->
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- TODO REMOVE PREVIOUS FONT & move fonts to the css/app.css file
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:100,600"> -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Abel|Cinzel+Decorative|Cinzel+Decorative:bold|Cinzel+Decorative:700">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
-        <!-- Fonts -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script
-          src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-          integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-          crossorigin="anonymous"></script>
-        <script>
-          window.Laravel = {!! json_encode([
-              'csrfToken' => csrf_token(),
-          ]) !!};
-        </script>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <link href="/css/app.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- PAGE: Title -->
+    <title>BookSwap | Home</title>
 
-        <!-- Styles -->
-        <style>
-            input[type=textz] {
-              width: 800px;
-              box-sizing: border-box;
-              border: 2px solid #ccc;
-              border-radius: 4px;
-              font-size: 16px;
-              background-color: white;
-              background-image: url('searchicon.png');
-              background-position: 10px 10px;
-              background-repeat: no-repeat;
-              padding: 12px 20px 12px 40px;
-              -webkit-transition: width 0.4s ease-in-out;
-              transition: width 0.4s ease-in-out;
-              }
-              input[type=text] {
-                font-weight: bold;
-                text-transform: capitalize;
-              }
+    <!-- IMPORT: jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script
+    src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+    integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+    crossorigin="anonymous"></script>
 
-            html, body {
-                background-color: #FFFFFF;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- CODE: Laravel (token) -->
+    <script>
+    window.Laravel = {!! json_encode([
+        'csrfToken' => csrf_token(),
+    ]) !!};
+    </script>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- CSS: Styles (believed to be a copy/paste of all styles for all pages) -->
+    <style>
+    html, body {
+        background-color: #333333;
+        color: #CCCCCC;
+        font-family: 'Abel', sans-serif;
+        width: 95%;
+        margin: auto;
+        min-witdh: 800px;
+        /* NOTE: box floats/aligns to top without all of the below...
+        Moved from random classes to main & header, now only position remains*/
+        height: 100vh;
+        align-items: center;
+        /* possible reason */
+        display: flex;
+        justify-content: center;
+    }
+    header, footer {
+        position: absolute;
+        padding: .2rem;
+        padding: 10px 10px;
+    }
+    header {
+        font-size: 1.2em;
+        float: right;
+        top: 0;
+        right: 0;
+        position: top;
+    }
+    footer {
+        font-size: .8em;
+        bottom: 0;
+        left: 0;
+        position: bottom;
+    }
+    main {
+        text-align: center;
+        position: relative;
+    }
+    .links > a {
+        color: #666666;
+        padding: 0 20px;
+        font-size: 1.2em;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .links > a:hover {
+        color: #AAFF00;
+        text-decoration: none;
+    }
+    #currentlink {
+        color: #AAFF00;
+    }
+    #logo-img {
+        width: 400px;
+    }
+    #logo-name {
+        padding-top: 15px;
+        padding-bottom: 30px;
+        font-size: 6em;
+        color: #77BB00;
+    }
+    #logo-name > span {
+        font-weight: 700;
+        font-family: 'Cinzel Decorative', cursive;
+    }
+    .form-control {
+        padding-top: 45px;
+        background-color: #333;
+        border: 0;
+        border-bottom: 3px solid #ccc;
+        outline: 0;
+        box-shadow: none;
+        font-weight: bold;
+    }
+    input[type=text] {
+        font-size: 1.2em;
+        font-weight: bold;
+        text-transform: capitalize;
+    }
+    textarea:focus, input[type="text"]:focus, .uneditable-input:focus {
+        border: 0;
+        border-bottom: 3px solid #403737;
+        border-radius: 0;
+        outline: 0;
+        box-shadow: none;
+        transition: all 300ms ease-in-out;
+    }
+    </style>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- CODE: JS (search functionality)-->
+    <script>
+    var books;
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .form-control {
-         border: 0;
-            border-bottom: 3px solid #403737;
-            border-radius: 0;
-            outline: 0;
-            box-shadow: none;
-            }
-            .form-control::-webkit-input-placeholder,
-            .form-control::-moz-input-placeholder,
-            .form-control:-ms-input-placeholder {
-         color: rgba(0, 0, 0, 0.8);
-            font-weight: bold;
-            }
-            textarea:focus,
-            input[type="text"]:focus,
-          .uneditable-input:focus {
-         border: 0;
-            border-bottom: 3px solid #403737;
-            border-radius: 0;
-            outline: 0;
-            box-shadow: none;
-            transition: all 300ms ease-in-out;
-            }
-
-         ::-webkit-input-placeholder {
-            color: rgba(0, 0, 0, 1.0);
-            font-weight: bold;
-          }
-          .footer {
-            position: absolute;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            padding: .2rem;
-            background-color: #efefef;
-            text-align: center;
-          }
-          .footer ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-            }
-
-           .footer li {
-                float: left;
-            }
-
-            .footer li a {
-                display: block;
-                color: black;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-            }
-            .footer li a:hover {
-                text-decoration: none;
-              }
-            .disabled {
-               pointer-events: none;
-               cursor: default;
-            }
-
-        </style>
-        <script>
-
-        var books;
-
-        function submitQuery(request, callback)
-        {
-          console.log("submitQuery called");
-              $.ajax({
-          method: "GET",
-          url: "/listexchangeJSON",   //getting JSON
-          data: { title: request.term }
-                //get title of book from written keyword
-
+    function submitQuery(request, callback)
+    {
+        console.log("submitQuery called");
+        $.ajax({
+            method: "GET",
+            url: "/listexchangeJSON",   //getting JSON
+            data: { title: request.term }
+            //get title of book from written keyword
         })
-          .done(function handleResponse(response) {
-
+        .done(function handleResponse(response) {
             console.log("handleResponse called: " + response);
             //create new array books
             books = response;
-
             var titles = new Array;
 
-          for (var i = 0; i < response.length; i++) {
-            titles.push(response[i].title);  //push title info into books
+            for (var i = 0; i < response.length; i++) {
+                titles.push(response[i].title);  //push title info into books
 
-            // in production code, item.text should have the HTML entities escaped.
-          //  document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
-          }
-
-        callback(titles);
-
-      console.log(titles);
-
-          });
-          //after 2 characters written potential titles of books are displayed in textbox
-
+                // in production code, item.text should have the HTML entities escaped.
+                //  document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
             }
 
+            callback(titles);
+            console.log(titles);
+        });
+        //after 2 characters written potential titles of books are displayed in textbox
+    }
 
-      $(function() {
-         $( "#search" ).autocomplete({
-             source: submitQuery,
-             minLength: 2,
-             select: function( event, ui ) {
-               // books.items[ui.item.id].volumeInfo.title
-               console.log( "Selected: " +  ui.item.value);
-               window.location.href = '/test?title=' + ui.item.value;
-             },
-
-           });
-           // enter keypress
-           $('#search').keypress(function (e) {
-             if (e.which == 13) {
-               //console.log("enter keypress" +  $('#search')[0].value );
+    $(function() {
+        $( "#search" ).autocomplete({
+            source: submitQuery,
+            minLength: 2,
+            select: function( event, ui ) {
+                // books.items[ui.item.id].volumeInfo.title
+                console.log( "Selected: " +  ui.item.value);
+                window.location.href = '/test?title=' + ui.item.value;
+            },
+        });
+        // enter keypress
+        $('#search').keypress(function (e) {
+            if (e.which == 13) {
+                //console.log("enter keypress" +  $('#search')[0].value );
                 window.location.href = '/test?title=' + $('#search')[0].value;
-             }
+            }
+        });
+    });
+    </script>
+    <!--<script src="/js/app.js"></script>-->
+</head>
+<body>
+    <!-- PAGE SECTION: Header -->
+    <header>
+        @if (Route::has('login'))
+        <div class="links">
+            <a href="{{ url('/welcome') }}"><span id="currentlink">Home</span></a>
+            <a href="{{ url('/livesearch') }}">Search</a>
+            <a href="{{ url('/about') }}">About</a>
+            <a href="{{ url('/faq') }}">FAQ</a>
 
+            @if (Auth::check())
+            <a href="{{ url('/selling') }}">Sell</a>
+            <a href="{{ url('/profile') }}">{{ Auth::user()->name }}</a>
 
-           });
-         });
-
-
-
-        </script>
-          <!--<script src="/js/app.js"></script>-->
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-
-                        <a href="{{ url('/test') }}">Explore</a>
-                        <a href="{{ url('/selling') }}">Sell</a>
-                        <a href="{{ url('/about') }}">About us</a>
-
-                        <a href="{{ url('/profile') }}"> {{ Auth::user()->name }}  </a>
-
-
-                    @else
-                        <a href="{{ url('/test') }}">Explore</a>
-                        <a href="{{ url('/about') }}">About Us</a>
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
+            @else
+            <a href="{{ url('/login') }}">Login</a>
+            <a href="{{ url('/register') }}">Register</a>
             @endif
+        </div>
+        @endif
+    </header>
 
-            <div class="content">
-              <div class="title m-b-md">
-                  <img src="{{URL::asset('img/BookSwapLogo.jpg')}}" alt="" style="width:500px;height:375px;"/>
-                  <input type="text" class="form-control" id="search" name="search" placeholder="Search book..">
-              </div>
+    <!-- PAGE SECTION: Main -->
+    <main>
+        <!-- MAIN SECTION: Logo -->
+            <img id="logo-img" src="{{URL::asset('img/logo.png')}}" alt="BookSwap logo. A green book."/><br>
+            <!-- <span class="title-book"> changes to Abel> -->
+            <div id="logo-name">BOOK<span>SWAP</span>
             </div>
+        <!-- MAIN SECTION: Search field -->
+        <div>
+            <input type="text" class="form-control" id="search" name="search" placeholder="Search book..">
         </div>
-        <div class="footer">
-          <ul>
-            <li><a href="{{ url('/home') }}">Home</a></li>
-            <li><a href="{{ url('/about') }}">About</a></li>
-            <li><a href="{{ url('/faq') }}">FAQ</a></li>
-            <li style="float:right"><a class="disabled" href="#copyright">Copyright &copy; 2017 <strong>BookSwap</strong>. All Rights Reserved.</a></li>
-          </ul>
-        </div>
-    </body>
+    </main>
+
+    <!-- PAGE SECTION: Footer -->
+    <footer>
+        Copyright &copy; 2017 <strong>BookSwap</strong>. All Rights Reserved.
+    </footer>
+</body>
 </html>

5459165775d914661266fd8ad0d0ea1d5cca25d7
Chelsea D'Alessandro <cdalessandro@ggc.edu>
2017-11-30 03:57:27 -0500

    Updated the other 'sell' page, addexchange.  Where users add books to sell

--- bookswap/resources/views/addExchange.blade.php
+++ bookswap/resources/views/addExchange.blade.php
@@ -3,48 +3,43 @@
 NOTE: this consists of the repetitive html & import information -->
 <title>BookSwap | Sell</title>
 
-<style>
-.radio-wrapper {
-    margin-left: 40px;
-}
-.label-class {
-    margin: 10px 0 0 0;
-}
-</style>
-
 @section('content')
 <div class="container">
-    <div class="col-sm-8 col-sm-offset-2">
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
 
-        <div class="panel">
-            <div class="panel-heading"><h1>Sell</h1></div>
-            <div class="panel-body">
-                <!-- DIFFERENT THAN PAGES login & register. code below
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+            <div class="panel">
+                <div class="panel-heading"><h1>Sell</h1></div>
+                <div class="panel-body">
+                    <!-- DIFFERENT THAN PAGES login & register. code below
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                     {{ csrf_field() }} -->
-                <form method="POST" action="/addExchange">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <label class="label-class" for="description">Description</label><br/>
-                        <!-- this should use BOOTSTRAP, not margin -->
-                        <div class="radio-wrapper">
-                            <input type="hidden" name="bookID" value={{$news}} >
-                            <input type="radio" name="desc" value="bad"> Bad
-                            <input type="radio" name="desc" value="fair"> Fair
-                            <input type="radio" name="desc" value="good"> Good <br/> <br/>
-                            <textarea class="textarea" name="description" rows="3" cols="80" maxlength="150" id="description" placeholder="Enter description in less than 150 characters"></textarea>
+                    <form class="form-horizontal" role="form" method="POST" action="/addExchange">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="description" class="col-sm-4 control-label">Description</label><br/>
+                            <!-- this should use BOOTSTRAP, not margin -->
+                            <div class="col-sm-6">
+                                <input type="hidden" name="bookID" value={{$news}} >
+                                <input type="radio" name="desc" value="bad" class="radio"> Bad
+                                <input type="radio" name="desc" value="fair" class="radio"> Fair
+                                <input type="radio" name="desc" value="good" class="radio"> Good <br/> <br/>
+                                <textarea class="textarea" name="description" rows="3" cols="80" maxlength="150" id="description" placeholder="Enter description in less than 150 characters"></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Price</label>
-                        <!-- NOTE - changed type from text to number, less likely to break -->
-                        <input type="number" class="form-control" id="price" name="price" placeholder="Enter amount only (i.e. 15.00 or 5)">
-                    </div>
-                    <div class="form-group btn-center">
-                        <button type="submit" class="btn">Submit</button>
-                    </div>
-                    @include('layouts.errors')
-                </form>
+                        <div class="form-group">
+                            <label for="price" class="col-sm-4 control-label">Price</label>
+                            <!-- NOTE - changed type from text to number, less likely to break -->
+                            $<input type="number" class="form-control" id="price" name="price" placeholder="Enter amount only, no '$' (i.e. 15.00 or 15)">
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-6 col-sm-offset-4">
+                                <button type="submit" class="btn">Submit</button>
+                            </div>
+                        </div>
+                        @include('layouts.errors')
+                    </form>
+                </div>
             </div>
         </div>
     </div>

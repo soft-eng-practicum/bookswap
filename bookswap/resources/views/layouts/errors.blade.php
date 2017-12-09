f0c6931cb3ab5813cb9e385c18699e5d93bed454
Chelsea D'Alessandro <cdalessandro@ggc.edu>
2017-12-09 14:23:40 -0500

    I broke the sell page from the last errors.blade.php update. This fixes that and comments were added at the top to describe the file's usage to prevent future problems.

--- bookswap/resources/views/layouts/errors.blade.php
+++ bookswap/resources/views/layouts/errors.blade.php
@@ -1,30 +1,13 @@
-@extends('layouts.app')
-<!-- FILE LOCATION: resources\views\layouts\app.blade.php
-NOTE: this consists of the repetitive html & import information -->
-<title>BookSwap | Error Page</title>
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-10 col-sm-offset-1">
-
-            <div class="panel">
-                <div class="panel-heading"><h1>Something Went Wrong</h1></div>
-                <div class="panel-body">
-                    @if(count($errors))
-                    <div class="form=group">
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li> {{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                    @endif
-                </div>
-            </div>
-
-        </div>
+<!-- NOTE - like app.blade.php, this is a blade temnplate file.
+It is used for errors, listing them, DO NOT format like a page  -->
+@if(count($errors))
+<div class="form=group">
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li> {{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
 </div>
-@endsection
+@endif
